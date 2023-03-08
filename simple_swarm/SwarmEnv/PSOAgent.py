154cf71576e81d1308757c4ed26fc2f009d4aa7c
DebjyotiRay <33850567+DebjyotiRay@users.noreply.github.com>
2023-03-08 13:07:27 +0530

    NEW CHANGES

---
+++ simple_swarm/SwarmEnv/PSOAgent.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+class PSOAgent:
+    def __init__(self, idx, position):
+        self.agent_id = idx
+        self.position = np.array(position).astype(int)
+        self.fitness_value = np.inf
+        self.P_best_pos = np.array(position).astype(int)
+        self.max_velocity = 20
+        self.velocity = np.array((0, 0))
+        
+    def get_velocity(self):
+        return self.velocity
+        
+    def get_position(self):
+        return self.position
+        
+    def update_velocity(self, swarm):
+        v_t = self.get_velocity()
+                
+        for i in range(0, swarm.num_dim):
+            r1 = np.random.uniform(0, 1, 1)
+            r2 = np.random.uniform(0, 1, 1)
+            
+            cog_dist = self.P_best_pos[i]-self.position[i]
+            soc_dist = swarm.G_best_pos[i]-self.position[i]
+            vel = swarm.inertia*v_t[i] + swarm.cognitive_weight*r1*cog_dist + swarm.social_weight*r2*soc_dist
+            
+            if vel > self.max_velocity:
+                vel = self.max_velocity
+            
+            elif vel < 0:
+                vel = 0
+                
+            self.velocity[i] = vel
+            
+    def update_position(self, swarm):
+        print('velocity:',self.get_velocity())
+        '''for i in range(0, swarm.num_dim):
+            dv = self.velocity[i]
+            
+            pos_t = self.position[i]
+            
+            if i == 0:#
+                if pos_t + dv < 20 or pos_t + dv > swarm.screen_width - 20:
+                    dv = -dv
+            else:
+                if pos_t + dv < 15 or pos_t + dv > swarm.screen_height - 50:
+                    dv = -dv
+            
+            self.position[i] = int(pos_t + dv)'''
---
+++ simple_swarm/pso_main.py
@@ -0,0 +1,33 @@
+from SwarmEnv.PSOEnv import PSOEnv
+
+def main():
+    swarm = PSOEnv(3, 0.002, 0.001, 1.0)
+    epoch = 0
+    while swarm.running:
+        if not swarm.convergence_criteria:
+            for i in range(swarm.no_agents):
+                curr_pos = swarm.PSOAgents[i].get_position()
+                value = swarm.objective_function(curr_pos)
+                
+                if value < swarm.PSOAgents[i].fitness_value:
+                    swarm.PSOAgents[i].fitness_value = value
+                    swarm.PSOAgents[i].P_best_pos = curr_pos
+            
+            swarm.G_best_pos, swarm.G_best_val, swarm.G_best_id = swarm.find_global_best()
+            
+            for i in range(swarm.no_agents):
+                swarm.PSOAgents[i].update_velocity(swarm)
+                print('velocity:',swarm.PSOAgents[i].get_velocity())
+                #swarm.PSOAgents[i].update_position(swarm)
+                
+            print('EPOCH:', epoch)
+            [print('Agent',i,':', swarm.PSOAgents[i].position, 'Fitness Value:',swarm.PSOAgents[i].fitness_value, 'Velocity:', swarm.PSOAgents[i].velocity) for i in range(swarm.no_agents)]
+            print('Global Best Agent: Agent', swarm.G_best_id)
+            print('Global Best Position:', swarm.G_best_pos)
+            print('Global Best Fitness Value:',swarm.G_best_val)
+            print('=================================================')
+        
+        epoch += 1
+
+if __name__ == '__main__':
+    main()
